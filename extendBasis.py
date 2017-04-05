9136224e3bce0b14ea056a86e61df4931b7646cd
Anas Khan <anas.khan96@outlook.com>
2017-04-05 18:17:53 +0530

    correct formatting

--- extendBasis.py
+++ extendBasis.py
@@ -3,40 +3,43 @@ dim = int(raw_input("Enter the number of dimensions :"))
 matrix = []
 print "Enter the matrix : "
 for i in range(dim):
-	vector = list(map(int,raw_input().split()))
-	matrix.append(vector)
+    vector = list(map(int, raw_input().split()))
+    matrix.append(vector)
 
 print
 print "Starting the extension -------------------"
 print
 
-def extend(matrix,n):
-	orthonorm = gramSchmidt(matrix,n)
-	ext_matrix = []
-	for vector in orthonorm:
-		x = vector.tolist()
-		x.append(0)
-		ext_matrix.append(x)
-	zeros = np.zeros(n)
-	y = zeros.tolist()
-	y.append(1)
-	ext_matrix.append(y)
-	return ext_matrix
 
-def gramSchmidt(matrix,n):
-	orthonorm = []
-	for vector in matrix:
-		b = vector
-		zeros = np.zeros(n)
-		for v in orthonorm:
-			zeros += np.dot(b,v)/np.dot(v,v)*v
-		orthonorm.append((b-zeros)/np.dot(b-zeros,b-zeros)**0.5)
-	return np.array(orthonorm)
+def extend(matrix, n):
+    orthonorm = gramSchmidt(matrix, n)
+    ext_matrix = []
+    for vector in orthonorm:
+        x = vector.tolist()
+        x.append(0)
+        ext_matrix.append(x)
+    zeros = np.zeros(n)
+    y = zeros.tolist()
+    y.append(1)
+    ext_matrix.append(y)
+    return ext_matrix
 
-for i in range(dim,10):
-	print i+1
-	print "--------------------------------------"
-	new_matrix = extend(matrix, i)
-	print np.array(new_matrix)
-	print "--------------------------------------"
-	matrix = new_matrix
+
+def gramSchmidt(matrix, n):
+    orthonorm = []
+    for vector in matrix:
+        b = vector
+        zeros = np.zeros(n)
+        for v in orthonorm:
+            zeros += np.dot(b, v) / np.dot(v, v) * v
+        orthonorm.append((b - zeros) / np.dot(b - zeros, b - zeros)**0.5)
+    return np.array(orthonorm)
+
+
+for i in range(dim, 10):
+    print i + 1
+    print "--------------------------------------"
+    new_matrix = extend(matrix, i)
+    print np.array(new_matrix)
+    print "--------------------------------------"
+    matrix = new_matrix
--- fixedPoint.py
+++ fixedPoint.py
@@ -2,21 +2,23 @@ import numpy as np
 
 # Given input data
 interval = [1, 2]
-polynomial = [1, 4, 0, -10] # x**3 + 4*x**2 - 10
+polynomial = [1, 4, 0, -10]  # x**3 + 4*x**2 - 10
 
 p = np.poly1d(polynomial)
-q = p.deriv() # derivative of the polynomial
-count = 0 # counting the iterations
+q = p.deriv()  # derivative of the polynomial
+count = 0  # counting the iterations
+
 
 def g(x):
     global count
-    result = x - (p(x)/float(q(x)))
+    result = x - (p(x) / float(q(x)))
     diff = abs(result - x)
     count += 1
-    if diff < 0.001: # taking 10^-3 as tolerance limit
+    if diff < 0.001:  # taking 10^-3 as tolerance limit
         print result
     else:
         g(result)
 
-g(interval[0]) # calling the function to start the iteration
+
+g(interval[0])  # calling the function to start the iteration
 print count
