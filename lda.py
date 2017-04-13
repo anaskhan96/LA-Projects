d67ba32982ff3a15abd341d5dbcc55bd6ecad28a
Anas Khan <anas.khan96@outlook.com>
2017-04-13 15:40:22 +0530

    changes

--- lda.py
+++ lda.py
@@ -9,18 +9,19 @@ classType = np.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
 def find_pivot(f1, f2):
     f1 = np.array(f1)
     f2 = np.array(f2)
-    pivot_arr = calculate_pivot(f1,f2)
-    end_pivot = reduce(lambda x,y: (x-y)/2, pivot_arr)
+    pivot_arr = calculate_pivot(f1, f2)
+    end_pivot = reduce(lambda x, y: (x - y) / 2, pivot_arr)
     return end_pivot
 
 
-def calculate_pivot(f1,f2):
-    return (f1-f2)/2
+def calculate_pivot(f1, f2):
+    return (f1 - f2) / 2
+
 
 x = float(raw_input('Enter the value of feature 1 :'))
 y = float(raw_input('Enter the value of feature 2 :'))
 pivot = find_pivot(feature1, feature2)
-point = calculate_pivot(x,y)
+point = calculate_pivot(x, y)
 if point < pivot:
     print 'Class 0'
 else:
