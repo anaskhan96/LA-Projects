4d28361f02f05785973383ba6346f2fc6b5118b8
Anas Khan <anas.khan96@outlook.com>
2017-04-19 12:43:20 +0530

    updated fixedPoint.py to measure count vs tolerance

--- fixedPoint.py
+++ fixedPoint.py
@@ -7,12 +7,14 @@ polynomial = [1, 4, 0, -10]  # x**3 + 4*x**2 - 10
 p = np.poly1d(polynomial)
 q = p.deriv()  # derivative of the polynomial
 count = 0  # counting the iterations
+diff_list = []
 
 
 def g(x):
     global count
     result = x - (p(x) / float(q(x)))
     diff = abs(result - x)
+    diff_list.append(diff)
     count += 1
     if diff < 0.001:  # taking 10^-3 as tolerance limit
         print result
@@ -22,3 +24,4 @@ def g(x):
 
 g(interval[0])  # calling the function to start the iteration
 print count
+print diff_list
